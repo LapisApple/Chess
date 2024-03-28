8af7ace1e7eac09d5b484ab364d5c77f05a14858
LapisApple <53429295+LapisApple@users.noreply.github.com>
2024-03-28 16:59:38 +0100

    Perft into main

--- src/main.cpp
+++ src/main.cpp
@@ -4,4 +4,94 @@
 
 #include "MainGameLoop.h"
 
-int main() { Chess::mainGameLoop(); }
+
+#include <cstdint>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "../Test/Main_Folder_Path_For_Testing.h"
+
+// Perft without Undo
+inline uint64_t Perft_impl(Board board, int depth, Team::Team team, Move prev_move) {
+    Team::Team enemy = Team::getEnemyTeam(team);
+    // get the new board
+
+    board.movePiece(prev_move);
+    if (Check::isChecked(board, enemy, board.positions.getKingPos(enemy))) return 0;
+
+    if (depth <= 0) return 1;
+    uint64_t amount_boards = 0;
+
+    std::vector<Move> move_list;
+    PossibleMoves::getAllPossibleMoves(board, move_list, team);
+
+    for (const Move next_move : move_list) {
+        amount_boards += Perft_impl(board, depth - 1, enemy, next_move);
+    }
+    return amount_boards;
+}
+
+inline uint64_t Perft(Board board, int depth, Team::Team team) {
+    Team::Team enemy = Team::getEnemyTeam(team);
+    if (!board.positions.hasPiece(Team::WHITE, PieceType::KING) ||
+        !board.positions.hasPiece(Team::BLACK, PieceType::KING))
+        return 0;
+
+    if (depth <= 0) return 1;
+    uint64_t amount_boards = 0;
+
+    std::vector<Move> move_list;
+    PossibleMoves::getAllPossibleMoves(board, move_list, team);
+
+    for (const Move move : move_list) {
+        amount_boards += Perft_impl(board, depth - 1, enemy, move);
+    }
+    return amount_boards;
+}
+
+#define GARBAGE_DATA 3
+#define MAX_PERFT 4
+#define MIN_PERFT 1
+
+void atomicPerft() {
+    std::ifstream file(main_folder_path + "/Test/gtest/datasets/atomic_perft.txt");
+    assert(file.good());
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream line_stream(line);
+        std::string fen_str;
+
+        std::getline(line_stream, fen_str, ';');
+
+        Fen fen;
+        try {
+            fen = Fen::buildFenFromStr(fen_str);
+        } catch (const FenParsingException& fenException) {
+            std::cerr << fenException.what();
+            assert(false);
+        }
+
+        Board board = Board(fen);
+
+        std::string perft_str;
+        std::vector<uint64_t> perft_data = {0};
+        while (std::getline(line_stream, perft_str, ';')) {
+            int64_t perf_res = std::stoi(perft_str.substr(GARBAGE_DATA));
+            perft_data.push_back(perf_res);
+        }
+
+        for (uint64_t i = MIN_PERFT; (i < perft_data.size()) && (i <= MAX_PERFT); ++i) {
+            uint64_t perft = Perft(board, static_cast<int>(i), fen.current_player);
+            std::cout << perft;
+            assert(perft == perft_data[i]);
+        }
+    }
+}
+
+
+int main() {
+    atomicPerft();
+    Chess::mainGameLoop();
+}
